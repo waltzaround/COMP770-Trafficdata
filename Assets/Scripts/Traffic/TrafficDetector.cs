817156a1fe604f05473a2018b4bf37a510017a08
waltzaround <vxs4050@autuni.ac.nz>
2016-05-30 11:55:03 +1200

    Actual commit

---
+++ Assets/Scripts/Traffic/TrafficDetector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrafficDetector : MonoBehaviour
+{
+
+    /*
+    //Color[] colors = new Color[3];
+
+    void Start()
+    {
+        colors[0] = Color.red;
+        colors[1] = Color.yellow;
+        colors[2] = Color.green;
+        InvokeRepeating("colorChange", 0, 15f);
+    }
+
+    void colorChange()
+    {
+        GetComponent<Renderer>().material.color = Color.Lerp(Color.green, Color.red, 2f);
+        GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.yellow, 2f);
+        GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.yellow, 2f);
+        Debug.Log("OMG COLORS");
+    }
+    */
+    public float speed = 0.5f;
+
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = Color.Lerp(Color.green, Color.red, Mathf.PingPong(Time.time * speed, 1.0f));
+     //   GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.yellow, 2f);
+      //  GetComponent<Renderer>().material.color = Color.Lerp(Color.yellow, Color.green, 2f);
+    }
+}
+
+
---
+++ Assets/Scripts/Traffic/newtraffic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newtraffic : MonoBehaviour {
+
+    float timeLeft;
+    Color targetColor;
+
+    void Update()
+    {
+        if (timeLeft <= Time.deltaTime)
+        {
+            // transition complete
+            // assign the target color
+            GetComponent<Renderer>().material.color = targetColor;
+
+            // start a new transition
+            targetColor = new Color(Random.value, Random.value, Random.value);
+            timeLeft = 5.0f;
+        }
+        else
+        {
+            // transition in progress
+            // calculate interpolated color
+            GetComponent<Renderer>().material.color = Color.Lerp(GetComponent<Renderer>().material.color, targetColor, Time.deltaTime / timeLeft);
+
+            // update the timer
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
